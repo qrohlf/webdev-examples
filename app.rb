38824e9d20394f08200cc8c38ac31569e9e6983d
Quinn Rohlf <qr@qrohlf.com>
2014-10-05 14:59:37 -0700

    Set up app to use postgresql in production

--- app.rb
+++ app.rb
@@ -10,11 +10,16 @@ require 'bundler/setup'
 Bundler.require
 require './models/TodoItem'
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'sqlite3',
-  :database => 'db/development.db',
-  :encoding => 'utf8'
-)
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  ActiveRecord::Base.establish_connection(
+    :adapter  => 'sqlite3',
+    :database => 'db/development.db',
+    :encoding => 'utf8'
+  )
+end
+
 
 get '/' do
   @tasks = TodoItem.all.order(:due)
